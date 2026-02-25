6be7df05020d9136f039e714be1a129cad696ce9
Nurtilek <abdlhtv06@gmail.com>
2026-02-25 17:24:20 +0600

    refactor routing and add HomeScreen implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-import 'package:_kvartant/sign_in.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'splash_sc.dart';
+import 'sign_in.dart';
+import 'register.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,14 +25,33 @@ class MyApp extends StatelessWidget {
         initialRoute: '/splash',
         routes: {
           '/splash': (context) => const SplashScreen(),
-          '/home': (context) => const ExactSignInScreen(),
+          '/signin': (context) => const ExactSignInScreen(),
+          '/register': (context) => const RegisterScreen(),
+          '/home': (context) => const HomeScreen(),
         },
       ),
     );
   }
 }
 
-// // Главный экран приложения (замени на свой)
+// Главный экран приложения (замени на свой)
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Главная'),
+      ),
+      body: const Center(
+        child: Text('Добро пожаловать!'),
+      ),
+    );
+  }
+}
+
+// // // Главный экран приложения (замени на свой)
 // class HomeScreen extends StatelessWidget {
 //   const HomeScreen({super.key});
 
